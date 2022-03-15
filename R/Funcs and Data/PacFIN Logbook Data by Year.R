82649b081cd2a84e521748412db22d9805e067cd
John Wallace <John.Wallace@noaa.gov>
2022-03-14 23:11:38 -0700

    updated embeded import.sql()

--- R/Funcs and Data/PacFIN Logbook Data by Year.R
+++ R/Funcs and Data/PacFIN Logbook Data by Year.R
@@ -4,31 +4,40 @@ PacFIN.Logbook.Data.by.Year <- function(Year = 1981, stringsAsCharacter = T, dsn
 
   require(Hmisc)
 
-  import.sql <- function(SQL, VAR="", VAL="", File = F, dsn, uid, pwd, View.Parsed.Only = F)
-  {
-
-     # Example using SQL without an external file:
-     #   import.sql("Select * from pacfin.bds_sp where rownum < 11", dsn="PacFIN", uid=<User ID>, pwd=<password>)
-
+   import.sql <- function (SQL, VAR = "", VAL = "", File = FALSE, dsn = 'PacFIN', uid, pwd, host = "china.psmfc.org", port = 2045, svc = "pacfin.psmfc.org", 
+                        View.Parsed.Only = FALSE, Windows = .Platform$OS.type == "windows") 
+{ 
+"  # import.sql( 'Select * from pacfin.bds_sp where rownum < 11', dsn = 'PacFIN', host = 'china.psmfc.org', port = 2045, svc = 'pacfin.psmfc.org', uid='wallacej', pwd= PacFIN.PW )  "
+   
+    require(Hmisc)
+    if (Windows) 
         require(RODBC)
-        require(Hmisc)
+    else 
+        require(ROracle)
+   
+    if (File) 
+        SQL <- paste(scan(SQL, what = " ", quote = NULL, quiet = T), collapse = " ")
+        
+    SQL.Parsed <- Hmisc::sedit(SQL, VAR, VAL)
+    
+    if (View.Parsed.Only) {
+        cat(SQL.Parsed, "\n")
+        return(invisible())
+    }
+    if(Windows) {
+        CON <- RODBC::odbcConnect(dsn, uid = uid, pwd = pwd)
+        on.exit(RODBC::odbcClose(CON))
+        RODBC::sqlQuery(CON, query = SQL.Parsed, stringsAsFactors = FALSE)
+    } else {
+        connect.string <- paste(
+          "(DESCRIPTION=",
+          "(ADDRESS=(PROTOCOL=tcp)(HOST=", host, ")(PORT=", port, "))",
+          "(CONNECT_DATA=(SERVICE_NAME=", svc, ")))", sep = "")
+        CON <- ROracle::dbConnect(dbDriver("Oracle"), username = uid, password = pwd, dbname = connect.string)
+        ROracle::fetch(ROracle::dbSendQuery(CON, SQL.Parsed))
+    }
+}
 
-        if(File) {
-                SQL.Parsed <- sedit(paste(scan(SQL, what = " ", quote = NULL, quiet = T), collapse = " "), VAR, VAL)
-                if(View.Parsed.Only) {
-                        printf(SQL.Parsed)
-                        return(invisible())
-                }
-        }
-        else SQL.Parsed <- sedit(SQL, VAR, VAL)
-
-        SQL.Parsed <- sedit(SQL, VAR, VAL)
-
-        CON <- odbcConnect(dsn, uid = uid, pwd = pwd)
-	on.exit(odbcClose(CON))
-
-        sqlQuery(CON, query = SQL.Parsed)
-  }
 
  # Get logbook data
 

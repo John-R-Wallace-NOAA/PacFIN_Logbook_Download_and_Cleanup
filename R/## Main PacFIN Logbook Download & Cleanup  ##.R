9c95d6862174086cb65f2c92e281032d46ed1849
John Wallace <John.Wallace@noaa.gov>
2022-05-31 12:01:31 -0700

    Now all FMP species are added to LB base file

--- R/## Main PacFIN Logbook Download & Cleanup  ##.R
+++ R/## Main PacFIN Logbook Download & Cleanup  ##.R
@@ -6,7 +6,7 @@
 
 # Edit the working directory below as desired; the source()'d R code (see below) needs to be in this directory and the save()'d support 
 #      data frames (e.g. polygon files) will be put into the subdirectory 'Funcs and Data'.  All saved data frames will also be in 'Funcs and Data'.
-    
+
 setwd("C:/PacFIN Logbook Download & Cleanup") 
 dir.create('Funcs and Data') # If needed
 
@@ -40,13 +40,13 @@ dir.create('Funcs and Data') # If needed
 
 # ----- PacFIN Logbook Download & Cleanup -----
 
-# Source Code: Download LB data from PacFIN 1981-2018.R >> Create shortform type table (Dat) with given species in columns.R >> Define polygons of reasonable catch area.R >>
+# Source Code: Download LB data from PacFIN 1981-2020.R >> Create shortform type table (Dat) with given species in columns.R >> Define polygons of reasonable catch area.R >>
 #               Add missing Latlong centroid block locations.R >> Add DepthGIS.R >> Create BestDepth and GoodTow.R
 
-# Data file names: LBData.1981.2018.dmp >> LB Shortform unfiltered Dat 25 Mar 2019.dmp >> LB Polygons Dat 25 Mar 2019.dmp >> LB GIS Depths Dat 25 Mar 2019.dmp >> LB Shortform Final Dat 25 Mar 2019.dmp  >> LB ShortForm No Hake Strat 25 Mar 2019.dmp
-#     LB Shortform EEZ Dat 25 Mar 2019.dmp, used to be saved in 'Remove data not in the EEZ.R' for backup due to long run time, now only done as a good stopping point - fi needed.
+# Data file names: LBData.1981.2020.RData >> LB Shortform unfiltered Dat 12 Apr 2021.RData >> LB Polygons Dat 12 Apr 2021.RData >> LB GIS Depths Dat 12 Apr 2021.RData >> LB Shortform Final Dat 12 Apr 2021.RData  >> LB ShortForm No Hake Strat 12 Apr 2021.RData
+#     LB Shortform EEZ Dat 12 Apr 2021.RData, used to be saved in 'Remove data not in the EEZ.R' for backup due to long run time, now only done as a good stopping point - fi needed.
 
-# R objects: LBData.1981.2018 >> Dat >> Dat >> LB.ShortForm >> LB.ShortForm.No.Hake.Strat 
+# R objects: LBData.1981.2020 >> Dat >> Dat >> LB.ShortForm >> LB.ShortForm.No.Hake.Strat 
  
 # *** Note that, in practice, the sourced R code would be stepped through, not just sourced in its entirety. ***
  
@@ -54,47 +54,47 @@ dir.create('Funcs and Data') # If needed
      # Set PacFIN.Login and PacFIN.PW for PacFIN server
      PacFIN.Login <- "<Your PacFIN Login Name>"
      PacFIN.PW <- "**********"
-     source('Download LB data from PacFIN 1981-2018.R') 
+     source('Download LB data from PacFIN 1981-2020.R') 
         # Skip above
-        # base::load('Funcs and Data/LBData.1981.2018.dmp')  # LBData.1981.2018 in R 
+        # base::load('Funcs and Data/LBData.1981.2020.RData')  # LBData.1981.2020 in R 
         # Using the base's load() here since JRWToolBox::load() is slower.  JRWToolBox::load() shows information about the loaded data frame, but it is slower since it loads twice.
      
      
      # Create LB Shortform (Unique tows in rows - species catch in columns). Note that the data frame 'Dat' has all tows.  A data frame called 'LB.ShortForm' with only good tows is created below.
      source('Create shortform type table (Dat) with given species in columns.R')
         # Skip all above
-        # load("Funcs and Data/LB Shortform unfiltered Dat 25 Mar 2019.dmp")
+        # base::load("Funcs and Data/LB Shortform unfiltered Dat 12 Apr 2021.RData")
 
         
      # Remove data not in the EEZ
      source('Remove data not in the EEZ.R') 
           
-     # Define the coastwide and bank polygon only once using the EEZ polygon and data from 2010-2018 
+     # Define the coastwide and bank polygon only once using the EEZ polygon and data from 2010-2020 
      # ***** Unless this is changed by committee or there is some significant change in where fish are caught (major undersea earthquake?), then I see no reason to change 
      #        these polygons. I plan to use them in the future and they will be provided for others in GitHub *****     
-     source('Define polygons of reasonable catch area.R')  # The code for defining the polygons of reasonable catch area is in a FALSE 'if' statement in lieu of code to download polygons from GitHub.
+     source('Define polygons of reasonable catch area.R')  # The code for defining the polygons of reasonable catch area is in a FALSE 'if' statement with the default to use the existing polygons.
         # Skip all above
-        # load("Funcs and Data/LB Polygons Dat 25 Mar 2019.dmp") # Dat in R
+        # load("Funcs and Data/LB Polygons Dat 12 Apr 2021.RData") # Dat in R
 
         
      # Add missing lat/long centroid block locations and GIS depth
      source('Add missing Latlong centroid block locations.R')           
      source('Add DepthGIS.R')
         # Skip all above
-        # load("Funcs and Data/LB GIS Depths Dat 25 Mar 2019.dmp") # Dat in R
+        # load("Funcs and Data/LB GIS Depths Dat 12 Apr 2021.RData") # Dat in R
 
-        
+     
      # Create 'BestDepth' and 'GoodTow' columns
      # ******** LB.ShortForm is 'Dat' with only good tows (LB.ShortForm <- Dat[Dat$GoodTow, ]) ******
-     source('Create BestDepth and GoodTow.R')  # Looking for a reasonable midwater depth limit is here along with LB.ShortForm <- Dat[Dat$GoodTow, ]
+     Source('Create BestDepth and GoodTow.R')  # Looking for a reasonable midwater depth limit is here along with LB.ShortForm <- Dat[Dat$GoodTow, ]
         # Skip all of above
-        # load('Funcs and Data/LB Shortform Final 25 Mar 2019.dmp') # LB.ShortForm in R
+        # load('Funcs and Data/LB Shortform Final 12 Apr 2021.RData') # LB.ShortForm in R
 
         
-     # Add additional species to LB Shortform base file and remove the Hake Strategy tows (*** May want to leave in Hake tows for Sablefish ***)
+     # Add all FMP species to LB Shortform base file
         source('Add species to LB Shortform.R')
         # Skip all of above:
-        # load('Funcs and Data/LB ShortForm No Hake Strat 25 Mar 2019.dmp') # LB.ShortForm.No.Hake.Strat in R
+        # load('Funcs and Data/LB ShortForm No Hake Strat 13 Apr 2021.RData') # LB.ShortForm.No.Hake.Strat in R
         
         
         
